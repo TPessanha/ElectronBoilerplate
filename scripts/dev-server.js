84425e2e29716c309bb01e2c8d4cdf16660a5a2f
Tomas Pessanha <t.pessanha@campus.fct.unl.pt>
2018-07-28 20:45:15 +0100

    Improved dev-server.js

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -4,45 +4,79 @@ const config = require("../config/webpack.config.development");
 
 const ignoredFiles = require("react-dev-utils/ignoredFiles");
 const appPaths = require("../config/appPaths");
+const chalk = require("chalk");
+const {
+	choosePort,
+	createCompiler,
+	prepareProxy,
+	prepareUrls
+} = require("react-dev-utils/WebpackDevServerUtils");
 const { spawn } = require("child_process");
 
-const host = process.env.HOST || "0.0.0.0";
-const port = parseInt(process.env.PORT, 10) || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
 
 const options = {
-	port: port,
+	port: DEFAULT_PORT,
 	stats: "minimal",
-	host: host,
+	host: HOST,
 	inline: true,
 	hot: true,
 	compress: true,
 	watchOptions: {
 		ignored: ignoredFiles(appPaths.appNodeModules, appPaths.appSrc)
-	},
-	before() {
-		const argv = require("minimist")(process.argv.slice(2));
-		if (argv.startHot) {
-			spawn(
-				"npm",
-				["run", argv.startHot === true ? "start:dev" : argv.startHot],
-				{
-					shell: true,
-					env: process.env,
-					stdio: "inherit"
-				}
-			)
-				.on("close", code => {
-					if (argv.linkLife) process.exit(code);
-				})
-				.on("error", spawnError => console.error(spawnError));
-		}
 	}
 };
 
 webpackDevServer.addDevServerEntrypoints(config, options);
 const compiler = webpack(config);
-const server = new webpackDevServer(compiler, options);
+const devServer = new webpackDevServer(compiler, options);
 
-server.listen(port, host, () => {
-	console.log(`dev server listening on port ${port}`);
-});
+choosePort(HOST, DEFAULT_PORT)
+	.then(port => {
+		if (port == null) {
+			// We have not found a port.
+			return;
+		}
+		process.env.PORT = port;
+		// Launch WebpackDevServer.
+		devServer.listen(port, HOST, err => {
+			if (err) {
+				return console.log(err);
+			}
+			console.log(chalk.cyan("Starting the development server...\n"));
+
+			const argv = require("minimist")(process.argv.slice(2));
+			if (argv.startHot) {
+				spawn(
+					"npm",
+					[
+						"run",
+						argv.startHot === true ? "start:dev" : argv.startHot
+					],
+					{
+						shell: true,
+						env: process.env,
+						stdio: "inherit"
+					}
+				)
+					.on("close", code => {
+						if (argv.linkLife) process.exit(code);
+					})
+					.on("error", spawnError => console.error(spawnError));
+			}
+		});
+
+		["SIGINT", "SIGTERM"].forEach(function(sig) {
+			process.on(sig, function() {
+				devServer.close();
+				process.exit();
+			});
+		});
+	})
+	.catch(err => {
+		if (err && err.message) {
+			console.log(err.message);
+		}
+		process.exit(1);
+	});
